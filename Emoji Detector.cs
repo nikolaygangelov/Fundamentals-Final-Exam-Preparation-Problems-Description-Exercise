fd3db2dadc727746aff97fac3903166148715ed9
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-26 20:11:30 +0200

    Add files via upload

---
+++ Emoji Detector.cs
@@ -0,0 +1,51 @@
+﻿
+namespace _2._Emoji_Detector
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text.RegularExpressions;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            BigInteger threshold = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i]; 
+
+                if (char.IsDigit(currentChar))
+                {
+                    threshold *= int.Parse(currentChar.ToString());
+                }
+
+            }
+
+            string pattern = @"((::)|(\*\*))(?<emoji>[A-Z][a-z]{2,})\1";
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(input);
+
+            Console.WriteLine($"Cool threshold: {threshold}");
+            Console.WriteLine($"{matchCollection.Count} emojis found in the text. The cool ones are:");
+            
+            foreach (Match match in matchCollection)//::Smiley:: , **Tigers**, ::Mooning::, **Shy**
+            {
+                int sumOfAsci = 0;
+                foreach (char ch in match.Groups["emoji"].Value)//S
+                {
+                    sumOfAsci += (int)ch;//83...
+                }
+
+                if (sumOfAsci >= threshold)
+                {
+                    Console.WriteLine(match);
+                }
+
+            }
+        }
+    }
+}
